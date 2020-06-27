23aa74105cae7af4df06e1e0a327fe36674d4272
jaisriram14 <67512620+jaisriram14@users.noreply.github.com>
2020-06-28 00:13:45 +0530

    added by developer 1

--- src/main/java/com/javateche/app/get_demo/App.java
+++ src/main/java/com/javateche/app/get_demo/App.java
@@ -8,6 +8,9 @@ public class App
 	if(data.equals(Constant.DATA_TYPE)) 
 		System.out.println("welcome java teche");
 	}
+	public void addData(String data) {
+		System.out.println("data : "+ data);
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
