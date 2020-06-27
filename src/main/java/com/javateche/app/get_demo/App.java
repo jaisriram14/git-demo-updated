828844659337511cfcff18241d988232aef7e504
mallesh <malleshr131@gmail.com>
2020-06-27 21:06:34 +0530

    added get-demo project to local repository first commit

---
+++ src/main/java/com/javateche/app/get_demo/App.java
@@ -0,0 +1,16 @@
+package com.javateche.app.get_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void addString(String data) {
+		System.out.println("data : "+ data);
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
