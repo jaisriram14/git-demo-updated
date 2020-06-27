29cd5a498edd791c4fc3b401983036a9928b646d
mallesh <malleshr131@gmail.com>
2020-06-27 23:49:41 +0530

    added constant.java file and modified app.java file

--- src/main/java/com/javateche/app/get_demo/App.java
+++ src/main/java/com/javateche/app/get_demo/App.java
@@ -1,15 +1,11 @@
 package com.javateche.app.get_demo;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
 	public void addString(String data) {
 		System.out.println("data : "+ data);
 	
-	if(data.equals("java teche")) 
+	if(data.equals(Constant.DATA_TYPE)) 
 		System.out.println("welcome java teche");
 	}
     public static void main( String[] args )
---
+++ src/main/java/com/javateche/app/get_demo/Constant.java
@@ -0,0 +1,6 @@
+package com.javateche.app.get_demo;
+
+public class Constant {
+	public static String DATA_TYPE="jAVA TECHE";
+
+}
