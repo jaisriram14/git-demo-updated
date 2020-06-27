3d2226ae74fbd636465507cfc41e288ac5460a26
mallesh <malleshr131@gmail.com>
2020-06-27 21:16:10 +0530

    if statement is added tojava file

--- src/main/java/com/javateche/app/get_demo/App.java
+++ src/main/java/com/javateche/app/get_demo/App.java
@@ -8,6 +8,9 @@ public class App
 {
 	public void addString(String data) {
 		System.out.println("data : "+ data);
+	
+	if(data.equals("java teche")) 
+		System.out.println("welcome java teche");
 	}
     public static void main( String[] args )
     {
